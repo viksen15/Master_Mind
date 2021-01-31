387c04a41f72bcf25f78efe60afb4a1e910b2286
viksen <viksen_1234@hotmail.com>
2021-01-31 20:51:55 +0100

    Añadido jugada + boton comprobacion

---
+++ Maind_Master/Master.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace Maind_Master
+{
+    partial class Master
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColoresDisponibles = new System.Windows.Forms.GroupBox();
+            this.bolacolor6 = new System.Windows.Forms.PictureBox();
+            this.bolacolor5 = new System.Windows.Forms.PictureBox();
+            this.bolacolor4 = new System.Windows.Forms.PictureBox();
+            this.bolacolor3 = new System.Windows.Forms.PictureBox();
+            this.bolacolor2 = new System.Windows.Forms.PictureBox();
+            this.bolacolor1 = new System.Windows.Forms.PictureBox();
+            this.CombinacionSecreta = new System.Windows.Forms.GroupBox();
+            this.solucion1 = new System.Windows.Forms.PictureBox();
+            this.solucion4 = new System.Windows.Forms.PictureBox();
+            this.solucion2 = new System.Windows.Forms.PictureBox();
+            this.solucion3 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nivelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reiniciarPartidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comoJugarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bolas = new System.Windows.Forms.FlowLayoutPanel();
+            this.comprobaciones = new System.Windows.Forms.FlowLayoutPanel();
+            this.Bcheck = new System.Windows.Forms.Panel();
+            this.ColoresDisponibles.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor1)).BeginInit();
+            this.CombinacionSecreta.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.solucion1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solucion4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solucion2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solucion3)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ColoresDisponibles
+            // 
+            this.ColoresDisponibles.Controls.Add(this.bolacolor6);
+            this.ColoresDisponibles.Controls.Add(this.bolacolor5);
+            this.ColoresDisponibles.Controls.Add(this.bolacolor4);
+            this.ColoresDisponibles.Controls.Add(this.bolacolor3);
+            this.ColoresDisponibles.Controls.Add(this.bolacolor2);
+            this.ColoresDisponibles.Controls.Add(this.bolacolor1);
+            this.ColoresDisponibles.Location = new System.Drawing.Point(370, 27);
+            this.ColoresDisponibles.Name = "ColoresDisponibles";
+            this.ColoresDisponibles.Size = new System.Drawing.Size(184, 65);
+            this.ColoresDisponibles.TabIndex = 0;
+            this.ColoresDisponibles.TabStop = false;
+            this.ColoresDisponibles.Text = "Colores disponibles";
+            // 
+            // bolacolor6
+            // 
+            this.bolacolor6.Location = new System.Drawing.Point(150, 23);
+            this.bolacolor6.Name = "bolacolor6";
+            this.bolacolor6.Size = new System.Drawing.Size(22, 23);
+            this.bolacolor6.TabIndex = 5;
+            this.bolacolor6.TabStop = false;
+            // 
+            // bolacolor5
+            // 
+            this.bolacolor5.Location = new System.Drawing.Point(122, 23);
+            this.bolacolor5.Name = "bolacolor5";
+            this.bolacolor5.Size = new System.Drawing.Size(22, 23);
+            this.bolacolor5.TabIndex = 4;
+            this.bolacolor5.TabStop = false;
+            // 
+            // bolacolor4
+            // 
+            this.bolacolor4.Location = new System.Drawing.Point(94, 23);
+            this.bolacolor4.Name = "bolacolor4";
+            this.bolacolor4.Size = new System.Drawing.Size(22, 23);
+            this.bolacolor4.TabIndex = 3;
+            this.bolacolor4.TabStop = false;
+            // 
+            // bolacolor3
+            // 
+            this.bolacolor3.Location = new System.Drawing.Point(66, 23);
+            this.bolacolor3.Name = "bolacolor3";
+            this.bolacolor3.Size = new System.Drawing.Size(22, 23);
+            this.bolacolor3.TabIndex = 2;
+            this.bolacolor3.TabStop = false;
+            // 
+            // bolacolor2
+            // 
+            this.bolacolor2.Location = new System.Drawing.Point(38, 23);
+            this.bolacolor2.Name = "bolacolor2";
+            this.bolacolor2.Size = new System.Drawing.Size(22, 23);
+            this.bolacolor2.TabIndex = 1;
+            this.bolacolor2.TabStop = false;
+            // 
+            // bolacolor1
+            // 
+            this.bolacolor1.Location = new System.Drawing.Point(10, 23);
+            this.bolacolor1.Name = "bolacolor1";
+            this.bolacolor1.Size = new System.Drawing.Size(22, 23);
+            this.bolacolor1.TabIndex = 0;
+            this.bolacolor1.TabStop = false;
+            // 
+            // CombinacionSecreta
+            // 
+            this.CombinacionSecreta.Controls.Add(this.solucion1);
+            this.CombinacionSecreta.Controls.Add(this.solucion4);
+            this.CombinacionSecreta.Controls.Add(this.solucion2);
+            this.CombinacionSecreta.Controls.Add(this.solucion3);
+            this.CombinacionSecreta.Location = new System.Drawing.Point(380, 98);
+            this.CombinacionSecreta.Name = "CombinacionSecreta";
+            this.CombinacionSecreta.Size = new System.Drawing.Size(164, 62);
+            this.CombinacionSecreta.TabIndex = 1;
+            this.CombinacionSecreta.TabStop = false;
+            this.CombinacionSecreta.Text = "Combinacion secreta";
+            // 
+            // solucion1
+            // 
+            this.solucion1.Location = new System.Drawing.Point(30, 19);
+            this.solucion1.Name = "solucion1";
+            this.solucion1.Size = new System.Drawing.Size(22, 23);
+            this.solucion1.TabIndex = 4;
+            this.solucion1.TabStop = false;
+            // 
+            // solucion4
+            // 
+            this.solucion4.Location = new System.Drawing.Point(114, 19);
+            this.solucion4.Name = "solucion4";
+            this.solucion4.Size = new System.Drawing.Size(22, 23);
+            this.solucion4.TabIndex = 2;
+            this.solucion4.TabStop = false;
+            // 
+            // solucion2
+            // 
+            this.solucion2.Location = new System.Drawing.Point(58, 19);
+            this.solucion2.Name = "solucion2";
+            this.solucion2.Size = new System.Drawing.Size(22, 23);
+            this.solucion2.TabIndex = 1;
+            this.solucion2.TabStop = false;
+            // 
+            // solucion3
+            // 
+            this.solucion3.Location = new System.Drawing.Point(86, 19);
+            this.solucion3.Name = "solucion3";
+            this.solucion3.Size = new System.Drawing.Size(22, 23);
+            this.solucion3.TabIndex = 3;
+            this.solucion3.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(569, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nivelToolStripMenuItem,
+            this.opcionesToolStripMenuItem,
+            this.reiniciarPartidaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nivelToolStripMenuItem
+            // 
+            this.nivelToolStripMenuItem.Name = "nivelToolStripMenuItem";
+            this.nivelToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.nivelToolStripMenuItem.Text = "Nivel";
+            this.nivelToolStripMenuItem.Click += new System.EventHandler(this.nivelToolStripMenuItem_Click);
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            this.opcionesToolStripMenuItem.Click += new System.EventHandler(this.opcionesToolStripMenuItem_Click);
+            // 
+            // reiniciarPartidaToolStripMenuItem
+            // 
+            this.reiniciarPartidaToolStripMenuItem.Name = "reiniciarPartidaToolStripMenuItem";
+            this.reiniciarPartidaToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.reiniciarPartidaToolStripMenuItem.Text = "Reiniciar partida";
+            this.reiniciarPartidaToolStripMenuItem.Click += new System.EventHandler(this.reiniciarPartidaToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.comoJugarToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // comoJugarToolStripMenuItem
+            // 
+            this.comoJugarToolStripMenuItem.Name = "comoJugarToolStripMenuItem";
+            this.comoJugarToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.comoJugarToolStripMenuItem.Text = "Como jugar?";
+            this.comoJugarToolStripMenuItem.Click += new System.EventHandler(this.comoJugarToolStripMenuItem_Click);
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de...";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // bolas
+            // 
+            this.bolas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bolas.Location = new System.Drawing.Point(12, 27);
+            this.bolas.Name = "bolas";
+            this.bolas.Size = new System.Drawing.Size(121, 32);
+            this.bolas.TabIndex = 9;
+            // 
+            // comprobaciones
+            // 
+            this.comprobaciones.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.comprobaciones.Location = new System.Drawing.Point(201, 27);
+            this.comprobaciones.Name = "comprobaciones";
+            this.comprobaciones.Size = new System.Drawing.Size(121, 32);
+            this.comprobaciones.TabIndex = 10;
+            // 
+            // Bcheck
+            // 
+            this.Bcheck.Location = new System.Drawing.Point(148, 29);
+            this.Bcheck.Name = "Bcheck";
+            this.Bcheck.Size = new System.Drawing.Size(31, 26);
+            this.Bcheck.TabIndex = 11;
+            // 
+            // Master
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 450);
+            this.Controls.Add(this.Bcheck);
+            this.Controls.Add(this.comprobaciones);
+            this.Controls.Add(this.bolas);
+            this.Controls.Add(this.CombinacionSecreta);
+            this.Controls.Add(this.ColoresDisponibles);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Master";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MasterMind \\\\VIIKSEN//";
+            this.Load += new System.EventHandler(this.Master_Load);
+            this.ColoresDisponibles.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bolacolor1)).EndInit();
+            this.CombinacionSecreta.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.solucion1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solucion4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solucion2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solucion3)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox ColoresDisponibles;
+        private System.Windows.Forms.GroupBox CombinacionSecreta;
+        private System.Windows.Forms.PictureBox bolacolor6;
+        private System.Windows.Forms.PictureBox bolacolor5;
+        private System.Windows.Forms.PictureBox bolacolor4;
+        private System.Windows.Forms.PictureBox bolacolor3;
+        private System.Windows.Forms.PictureBox bolacolor2;
+        private System.Windows.Forms.PictureBox bolacolor1;
+        private System.Windows.Forms.PictureBox solucion1;
+        private System.Windows.Forms.PictureBox solucion4;
+        private System.Windows.Forms.PictureBox solucion2;
+        private System.Windows.Forms.PictureBox solucion3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nivelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reiniciarPartidaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comoJugarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel bolas;
+        private System.Windows.Forms.FlowLayoutPanel comprobaciones;
+        private System.Windows.Forms.Panel Bcheck;
+    }
+}
+
---
+++ Maind_Master/Master.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maind_Master
+{
+    public partial class Master : Form
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+        private void nivelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Niveles nivel = new Niveles();
+            nivel.Show();
+        }
+        private void opcionesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reiniciarPartidaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void comoJugarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La maquina escoge un número de fichas de colores, 4 en modo Principiante, 5 en modo Medio, 6 en modo Avanzado, y pone un código secreto oculto para el otro jugador. Este, tomando las fichas de colores, aventura una posibilidad." +
+                "\nContestada con negras (fichas de color bien colocadas). Blancas (fichas de color con el color correcto, pero mal colocadas)." +
+                "\nEl juego termina al averiguarse la combinación (es decir, se consigue una combinación con cuatro negras), o bien se agota el tablero");
+
+        }
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("BY VIIKSEN ®");
+        }
+
+        Random rnd = new Random();
+        public Color[] colores = new Color[] { Color.Red, Color.Blue, Color.Yellow, Color.Green, Color.DarkOrange, Color.HotPink};
+        PictureBox[] bolacolores;
+        Niveles nivel = new Niveles();
+        const int bolasMax = 4;
+        public int intentos;
+
+        private void crearColores()
+        {
+            if (nivel.nivel == 0)
+            {
+                bolacolores = new[] { bolacolor1, bolacolor2, bolacolor3, bolacolor4 };
+                for (int i = 0; i < bolacolores.Length; i++)
+                {
+                    bolacolores[i].BackColor = colores[i];
+                }
+            }
+
+            if (nivel.nivel == 1)
+            {
+                bolacolores = new[] { bolacolor1, bolacolor2, bolacolor3, bolacolor4, bolacolor5 };
+                for (int i = 0; i < bolacolores.Length; i++)
+                {
+                    bolacolores[i].BackColor = colores[i];
+                }
+            }
+
+            if (nivel.nivel == 2)
+            {
+                bolacolores = new[] { bolacolor1, bolacolor2, bolacolor3, bolacolor4, bolacolor5, bolacolor6 };
+                for (int i = 0; i < bolacolores.Length; i++)
+                {
+                    bolacolores[i].BackColor = colores[i];
+                }
+            }
+
+        }
+        private void crearSolucion()
+        {
+            PictureBox[] bolasolucion;
+            bolasolucion = new[] { solucion1, solucion2, solucion3, solucion4 };
+            for (int i=0; i<bolasolucion.Length;i++)
+            { bolasolucion[i].BackColor = colores[rnd.Next(bolacolores.Length)]; }
+        }
+
+        private void Master_Load(object sender, EventArgs e)
+        {
+            nivel.ShowDialog();
+            crearColores();
+            crearSolucion();
+            crearLineaBola();
+        }
+        private void crearLineaBola()
+        {
+            for (int i = 0; i < bolasMax; i++)
+            {
+                PictureBox bola = new PictureBox()
+                {
+                    BackColor = Color.White,
+                    Size = new Size(22, 22)
+                }; 
+                bolas.Controls.Add(bola);
+            }
+
+            Button check = new Button()
+            {
+                BackgroundImage = Properties.Resources.checkButton,
+                BackgroundImageLayout = ImageLayout.Stretch,
+                Size = new Size(25, 25)
+            };
+          //  check.Click += ComprobarClick;
+            Bcheck.Controls.Add(check);
+        }
+
+
+
+
+
+
+
+
+        /*
+        private void JugadaClick(object sender, EventArgs e)
+        {
+            Color seleccionado = ColoresDisponibles.BackColor;
+            int posicion = Array.IndexOf(bolacolores, seleccionado);
+        }*/
+
+
+
+
+    }
+}
